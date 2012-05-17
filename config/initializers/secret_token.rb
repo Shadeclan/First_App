931c684bd7207300e99acc5e79dfa8f267b4b908
Woody Barrett <Woody.Barrett@dos.ny.gov>
2012-05-17 14:19:49 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7a1fb7d8bcf940d4f9b57984ebfd848ffeae22cc8e8e7c2680be98ce991d85baae4ce694e620dbff2279f273d4ac5ebe9e8d32ca5dc3814fccbb366aeb86dd7c'
